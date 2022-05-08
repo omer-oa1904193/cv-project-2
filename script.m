9f72b6ff94988c2abc4628e7618fc169f00406aa
squivix <devordem@gmail.com>
2022-05-08 21:04:16 +0300

    Finish preparing data with following features extracted after segmentation: area, circularity, and eccentricity

--- script.m
+++ script.m
@@ -1,41 +1,89 @@
 function script()
-global HUE_MIN;
-global HUE_MAX;
+global GREEN_HUE_MIN;
+global GREEN_HUE_MAX;
 global SATURATION_MIN;
 global VALUE_MIN;
-HUE_MIN=65;
-HUE_MAX=165;
-SATURATION_MIN=15;
+GREEN_HUE_MIN=60;
+GREEN_HUE_MAX=165;
+SATURATION_MIN=8;
 VALUE_MIN=10;
-    x=1:30;
-    y=zeros(30);
+COLOR_TO_LUM_MIN_THRESH=130;
+    feature_1_x=zeros(30,1);
+    feature_2_y=zeros(30,1);
+    feature_3_z=zeros(30,1);
     for i=1:30
         img=imread(sprintf('images/%02d.jpg', i));
-        y(i)=count_green(img);
-        %imshow(segment_image(img));
+        green_count=count_green(img);
+        if green_count>=COLOR_TO_LUM_MIN_THRESH
+            img_bw=color_segment_image(img);
+            feature_1_x(i)=green_count;
+        else
+            img_bw=lum_segment_image(img);
+        end
+        % clean up image
+        img_bw=bwareafilt(img_bw,1);
+        img_bw=(~bwareafilt(~img_bw,1));
+        
+        imshow(img_bw)
+        img_lebeled = bwlabel(img_bw);
+        img_props = regionprops(img_lebeled, 'Perimeter','Circularity','Eccentricity');
+        feature_2_y(i)=img_props(1).Circularity;
+        feature_3_z(i)=img_props(1).Eccentricity;
+        
+        if green_count<COLOR_TO_LUM_MIN_THRESH
+            feature_1_x(i)=sum(sum(img_bw));
+        end
     end
-    bar(x, y, 'BarWidth');
+    feature_1_x=feature_1_x/max(feature_1_x);
+    feature_2_y=feature_2_y/max(feature_2_y);
+    feature_3_z=feature_3_z/max(feature_3_z);
+    
+    close all;
+    figure;
+    hold on;
+    scatter3(feature_1_x(1:10), feature_2_y(1:10),feature_3_z(1:10) ,'r.');
+    scatter3(feature_1_x(11:20), feature_2_y(11:20),feature_3_z(11:20) ,'g.');
+    scatter3(feature_1_x(21:30), feature_2_y(21:30),feature_3_z(21:30) ,'b.');
+    
+    xlabel('Feature 1');
+    ylabel('Feature 2');
+    zlabel('Feature 3');
+    hold off;
+    grid();
+    writematrix(feature_1_x,'feature_1.csv');
+    writematrix(feature_2_y,'feature_2.csv');
+    writematrix(feature_3_z,'feature_3.csv');
 end
 
 
 function out=count_green(img)
-global HUE_MIN;
-global HUE_MAX;
+global GREEN_HUE_MIN;
+global GREEN_HUE_MAX;
 global SATURATION_MIN;
 global VALUE_MIN;
 img=rgb2hsv(img);
-b = ((img(:,:,1)*360)>=HUE_MIN & (img(:,:,1)*360)<=HUE_MAX & (img(:,:,2)*100)>=SATURATION_MIN & (img(:,:,3)*100)>= VALUE_MIN);
+b = ((img(:,:,1)*360)>=GREEN_HUE_MIN & (img(:,:,1)*360)<=GREEN_HUE_MAX)&...
+    (img(:,:,2)*100)>=SATURATION_MIN & (img(:,:,3)*100)>= VALUE_MIN;
 out=sum(sum(sum(b)));
 end
 
-function out=segment_image(img)
-global HUE_MIN;
-global HUE_MAX;
+function out=color_segment_image(img)
+global GREEN_HUE_MIN;
+global GREEN_HUE_MAX;
 global SATURATION_MIN;
 global VALUE_MIN;
 img=rgb2hsv(img);
-mask =  ((img(:,:,1)*360)>=HUE_MIN & (img(:,:,1)*360)<=HUE_MAX & (img(:,:,2)*100)>=SATURATION_MIN & (img(:,:,3)*100)>= VALUE_MIN);
-out=hsv2rgb(img) .* mask;
+mask = ((img(:,:,1)*360)>=GREEN_HUE_MIN & (img(:,:,1)*360)<=GREEN_HUE_MAX)&...
+    (img(:,:,2)*100)>=SATURATION_MIN & (img(:,:,3)*100)>= VALUE_MIN;
+%out=hsv2rgb(img) .* mask;
+out=~mask;
+end
+
+function out=lum_segment_image(img)
+img_gray=rgb2gray(img);
+mask=uint8(~imbinarize(img_gray));
+%out=img.*mask;
+out=~mask;
 end
 
 
@@ -65,12 +113,12 @@ imshow(image);
 end
 
 function out = is_green(hsv_pixel)
-global HUE_MIN;
-global HUE_MAX;
+global GREEN_HUE_MIN;
+global GREEN_HUE_MAX;
 global SATURATION_MIN;
 global VALUE_MIN;
 hue=hsv_pixel(1)*360;
 saturation=hsv_pixel(2)*100;
 value=hsv_pixel(3)*100;
-out = (hue >= HUE_MIN && hue <= HUE_MAX && saturation>=SATURATION_MIN && value >= VALUE_MIN);
+out = (hue >= GREEN_HUE_MIN && hue <= GREEN_HUE_MAX && saturation>=SATURATION_MIN && value >= VALUE_MIN);
 end
